297ffee12f6a107cd6019337db24ce9afcfe8293
kubawieczorek <jwieczorek94@wp.pl>
2017-06-04 15:22:41 +0200

    Funkcja Dominiki
    Funkcja Dominiki

--- MPCACodes/src/MPCA_with_load.m
+++ MPCACodes/src/MPCA_with_load.m
@@ -1,12 +1,12 @@
 % funkcja ³aduje obrazy kategoii "faces", dodaje je do macierzy i uruchamia
 % algorytm MPCA
-function [tUs, odrIdx, TXmean, Wgt] = MPCA_with_load()
+function [tUs, odrIdx, TXmean, Wgt] = MPCA_with_load(cat_name, cat_id)
     for n = 1:50
         % wyznaczenie œcie¿ki
         if n < 10
-            path = strcat('/pictures/faces/image_000', num2str(n), '.jpg');
+            path = strcat('pictures/',cat_name,'/image_000', num2str(n), '.jpg');
         else
-            path = strcat('/pictures/faces/image_00', num2str(n), '.jpg');
+            path = strcat('pictures/',cat_name,'/image_00', num2str(n), '.jpg');
         end;
         % ³adowanie obrazka
         img = imread(path);
@@ -17,5 +17,5 @@ function [tUs, odrIdx, TXmean, Wgt] = MPCA_with_load()
             matrix4D = cat(4, matrix4D, img);
         end;
     end;
-    [tUs, odrIdx, TXmean, Wgt]  = MPCA(double(matrix4D),1,97,1);
+    [tUs, odrIdx, TXmean, Wgt]  = MPCA(double(matrix4D),cat_id,97,1);
 end
